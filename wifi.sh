62e2ad705b3cc7f801292aac282bf8ebd8944ef0
Toby Canning <toby@canning.us>
2016-02-06 16:00:42 -0600

    Added @jasaoncalabrese's cron line
    Auto switched back to home network when available

--- wifi.sh
+++ wifi.sh
@@ -25,7 +25,7 @@
 # once every 2 mins */5 to */2 ...
 #
 # */5 * * * * ~/src/edison_wifi/wifi.sh
-#
+# */15 * * * * ( (wpa_cli status | grep $YOUR_HOME_NETWORK > /dev/null && echo already on $YOUR_HOME_NETWORK) || (wpa_cli scan > /dev/null && wpa_cli scan_results | egrep $YOUR_HOME_NETWORK > /dev/null && wpa_cli select_network $(wpa_cli list_networks | grep $YOUR_HOME_NETWORK | cut -f 1) && echo switched to $YOUR_HOME_NETWORK && sleep 15 && (for i in $(wpa_cli list_networks | grep DISABLED | cut -f 1); do wpa_cli enable_network $i > /dev/null; done) && echo and re-enabled other networks) ) 2>&1 | logger -t wifi-select
 ##################################################################
 # Settings
 # Where and what you want to call the Lockfile
