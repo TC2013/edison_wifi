6525bfba59fb37d371c7c7ef3bc80d1b8a7b2fad
Jason Calabrese <jason@cbrese.com>
2016-02-06 16:56:33 -0800

    set pingip from the first script param instead of hardcoding

--- wifi.sh
+++ wifi.sh
@@ -24,9 +24,10 @@
 # this is a very common problem.  If once a min change */5 to *
 # once every 2 mins */5 to */2 ...
 #
-# */5 * * * * ~/src/edison_wifi/wifi.sh
-# sudo crontab -e  (to run cron from root)
+# REMOTE_HOST="google.com" # will be used to test network connectivity
 # YOUR_HOME_NETOWRK=ssid_name  #change ssid_name to your primary network you want to use.  This will switch to that network if it is available.
+# */5 * * * * ~/src/edison_wifi/wifi.sh $REMOTE_HOST
+# sudo crontab -e  (to run cron from root)
 # */15 * * * * ( (wpa_cli status | grep $YOUR_HOME_NETWORK > /dev/null && echo already on $YOUR_HOME_NETWORK) || (wpa_cli scan > /dev/null && wpa_cli scan_results | egrep $YOUR_HOME_NETWORK > /dev/null && wpa_cli select_network $(wpa_cli list_networks | grep $YOUR_HOME_NETWORK | cut -f 1) && echo switched to $YOUR_HOME_NETWORK && sleep 15 && (for i in $(wpa_cli list_networks | grep DISABLED | cut -f 1); do wpa_cli enable_network $i > /dev/null; done) && echo and re-enabled other networks) ) 2>&1 | logger -t wifi-select
 ##################################################################
 # Settings
@@ -34,7 +35,7 @@
 lockfile='~/src/edison_wifi/WiFi_Check.pid'
 # Which Interface do you want to check/fix
 wlan='wlan0'
-pingip='192.168.1.1'
+pingip=$1
 ##################################################################
 echo
 echo "Starting WiFi check for $wlan"
