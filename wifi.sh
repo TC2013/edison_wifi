ca8cfbd4f214cc306746e7ae437f03efc3fd7579
Toby Canning <toby@canning.us>
2016-04-23 16:34:50 -0500

    Update wifi.sh

--- wifi.sh
+++ wifi.sh
@@ -26,11 +26,11 @@
 #
 # 
 # sudo crontab -e  (to run cron from root) 
-# REMOTE_HOST="google.com" # will be used to test network connectivity
+# 
 # */5 * * * * ~/src/edison_wifi && ./wifi.sh $REMOTE_HOST
 #The line below is also meant to run in cron and is supposed to look and see if your home network is available then switch to it if it is.  Not sure it works
 # */15 * * * * ( (wpa_cli status | grep $YOUR_HOME_NETWORK > /dev/null && echo already on $YOUR_HOME_NETWORK) || (wpa_cli scan > /dev/null && wpa_cli scan_results | egrep $YOUR_HOME_NETWORK > /dev/null && wpa_cli select_network $(wpa_cli list_networks | grep $YOUR_HOME_NETWORK | cut -f 1) && echo switched to $YOUR_HOME_NETWORK && sleep 15 && (for i in $(wpa_cli list_networks | grep DISABLED | cut -f 1); do wpa_cli enable_network $i > /dev/null; done) && echo and re-enabled other networks) ) 2>&1 | logger -t wifi-select
-#
+# REMOTE_HOST="google.com" # will be used to test network connectivity
 # YOUR_HOME_NETOWRK=ssid_name  #change ssid_name to your primary network you want to use.  This will switch to that network if it is available.
 #################################################################
 # Settings
